f07714f21e63a826fea75e4be01ee69d0dfe6b48
Hotmoon <aravind1333.av@gmail.com>
2022-09-06 19:29:02 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -170,7 +170,7 @@ def get_readable_message():
                 msg += f"\n<b>Size: </b>{download.size()}"
                 msg += ''
             msg += f"\n<b>To Cancel: </b><code>/{BotCommands.CancelMirror} {download.gid()}</code>"
-            msg += "\n\n"
+            msg += "\n"
             if STATUS_LIMIT is not None and index == STATUS_LIMIT:
                 break
         if len(msg) == 0:
