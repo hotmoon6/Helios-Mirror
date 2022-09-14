46eb116cfa798cec5811295df0474580f097f9f6
Hotmoon <aravind1333.av@gmail.com>
2022-09-14 22:51:11 +0530

    Update torrent_search.py

--- bot/modules/torrent_search.py
+++ bot/modules/torrent_search.py
@@ -24,9 +24,9 @@ from pyrogram.parser import html as pyrogram_html
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.handlers import MessageHandler, CallbackQueryHandler
 
-from tobrot import app, dispatcher, bot, LOGGER 
-from tobrot.helper_funcs.bot_commands import BotCommands
-from tobrot.helper_funcs.filters import CustomFilters
+from bot import app, dispatcher, bot, LOGGER 
+from bot.helper_funcs.bot_commands import BotCommands
+from bot.helper_funcs.filters import CustomFilters
 
 search_lock = asyncio.Lock()
 search_info = {False: dict(), True: dict()}
