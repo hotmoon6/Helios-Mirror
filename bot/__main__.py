c642cc92ed2065575dd5bbc0b98cdff04bfa26f0
Hotmoon <aravind1333.av@gmail.com>
2022-09-15 16:01:08 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -228,12 +228,15 @@ def main():
                                   bot_help, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
     stats_handler = CommandHandler(BotCommands.StatsCommand,
                                    stats, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+    tshelp_handler = CommandHandler(BotCommands.TsHelpCommand,
+                                   searchhelp, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
     log_handler = CommandHandler(BotCommands.LogCommand, log, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(ping_handler)
     dispatcher.add_handler(restart_handler)
     dispatcher.add_handler(help_handler)
     dispatcher.add_handler(stats_handler)
+    dispatcher.add_handler(tshelp_handler)
     dispatcher.add_handler(log_handler)
     updater.start_polling(drop_pending_updates=IGNORE_PENDING_REQUESTS)
     LOGGER.info("Bot Started!")
