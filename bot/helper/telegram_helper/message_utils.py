5996b46fb9ad9fec6767792d6427f972968f5508
Hotmoon <aravind1333.av@gmail.com>
2022-09-15 23:05:19 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -164,9 +164,9 @@ def sendStatusMessage(msg, bot, update):
             deleteMessage(bot, message)
             del status_reply_dict[msg.chat.id]
         if buttons == "":
-            message = sendMessage(progress, bot, msg)
+            message = send_photo(progress, bot, msg, photo=(Pics), update.message)
         else:
-            message = send_photo(progress, bot, msg, buttons, photo=Pics, update.message)
+            message = send_photo(progress, bot, msg, buttons, photo=(Pics), update.message)
         status_reply_dict[msg.chat.id] = [message, time()]
         if not Interval:
             Interval.append(setInterval(DOWNLOAD_STATUS_UPDATE_INTERVAL, update_all_messages))
